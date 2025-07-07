85ff342cf79b71edd2cdb35996da29f4a07ad76e
Mazalov Alex <mazalovalexey1995@yandex.ru>
2025-07-07 16:23:06 +0300

    изменил админку(убрал валидацию)

--- schemaTypes/header/menu.js
+++ schemaTypes/header/menu.js
@@ -7,21 +7,18 @@ export default {
       name: 'title',
       title: 'Title',
       type: 'string',
-      validation: (Rule) => Rule.required(),
     },
     {
       name: 'url',
       title: 'URL',
       type: 'url',
       description: 'Ссылка, куда ведет пункт меню',
-      validation: (Rule) => Rule.required(),
     },
     {
       name: 'order',
       title: 'Order',
       type: 'number',
       description: 'Порядок отображения в меню',
-      validation: (Rule) => Rule.required().min(0),
     },
   ],
 }
