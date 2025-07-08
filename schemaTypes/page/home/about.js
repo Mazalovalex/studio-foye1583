56e7ce996f6eccf7c15947123b955cf29555c077
Mazalov Alex <mazalovalexey1995@yandex.ru>
2025-07-08 20:37:54 +0300

    добавил возможность редактировать О нас

--- schemaTypes/page/home/about.js
+++ schemaTypes/page/home/about.js
@@ -1,18 +1,46 @@
 export default {
-  name: 'about',
-  title: 'О нас',
+  name: 'aboutSection',
+  title: 'Блок «О нас»',
   type: 'document',
   fields: [
     {
-      name: 'title',
-      title: 'Заголовок',
-      type: 'string',
+      name: 'quote',
+      title: 'Цитата',
+      type: 'object',
+      fields: [
+        {name: 'text', title: 'Текст цитаты', type: 'string'},
+        {name: 'author', title: 'Автор', type: 'string'},
+      ],
     },
     {
-      name: 'content',
-      title: 'Основной текст',
+      name: 'paragraphs',
+      title: 'Абзацы',
       type: 'array',
-      of: [{type: 'block'}],
+      of: [{type: 'text'}],
+      validation: (Rule) => Rule.max(3),
+    },
+    {
+      name: 'links',
+      title: 'Ссылки (меню)',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {name: 'title', title: 'Текст ссылки', type: 'string'},
+            {name: 'url', title: 'URL', type: 'url'},
+          ],
+        },
+      ],
+      validation: (Rule) => Rule.max(2),
     },
   ],
+
+  preview: {
+    prepare() {
+      return {
+        title: 'О нас / Блок фойе',
+      }
+    },
+  },
 }
