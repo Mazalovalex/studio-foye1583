cc7a17fbec22ca8ac1de4b987185d8c851da83c9
Mazalov Alex <mazalovalexey1995@yandex.ru>
2025-07-10 12:18:05 +0300

    добавили слайдер атмосфера в админку

--- schemaTypes/page/home/sliderAtmosphere.js
+++ schemaTypes/page/home/sliderAtmosphere.js
@@ -1,6 +1,6 @@
 export default {
   name: 'sliderAtmosphere',
-  title: 'Главная | Слайдер Атмосфера',
+  title: 'Главная | Слайдер — Атмосфера',
   type: 'document',
   fields: [
     {
@@ -9,12 +9,36 @@ export default {
       type: 'array',
       of: [
         {
-          type: 'image',
-          name: 'image',
-          title: 'Изображение',
-          options: {hotspot: true},
+          type: 'object',
+          fields: [
+            {
+              name: 'image',
+              title: 'Изображение',
+              type: 'image',
+              options: {hotspot: true},
+            },
+            {
+              name: 'alt',
+              title: 'Альт-текст',
+              type: 'string',
+              validation: (Rule) => Rule.required().max(100),
+            },
+          ],
+          preview: {
+            select: {
+              media: 'image',
+              title: 'alt',
+            },
+          },
         },
       ],
     },
   ],
+  preview: {
+    prepare() {
+      return {
+        title: 'Слайдер: Атмосфера',
+      }
+    },
+  },
 }
